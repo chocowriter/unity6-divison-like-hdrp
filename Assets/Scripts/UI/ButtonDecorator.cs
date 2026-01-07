9ecd37c94417eb884b0fe7b8bc809c705be8dee1
chocowriter <chocowriter0@gmail.com>
2026-01-07 14:19:10 +0900

    로고 이미지 추가, 버튼 hover 되었을 때 효과 추가

---
+++ Assets/Scripts/UI/ButtonDecorator.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+
+namespace DivisionLike
+{
+    public class ButtonDecorator : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private Button m_Button;
+        private TextMeshProUGUI m_Text;
+        
+        void Awake()
+        {
+            m_Button = GetComponent<Button>();
+            m_Text = GetComponentInChildren<TextMeshProUGUI>();
+        }
+        
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            m_Text.color = Color.black;
+            this.transform.localPosition += new Vector3(0, 0f, -40f);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            m_Text.color = Color.white;
+            this.transform.localPosition += new Vector3(0, 0f, 40f);
+        }
+    }    
+}
